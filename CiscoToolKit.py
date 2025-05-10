6b407a2de508591cd64836913cfe6e801b5a954f
noxlumens <111706416+noxlumens@users.noreply.github.com>
2025-05-10 15:29:59 -0500

    Update CiscoToolKit.py
    Remove -A because -u already performs the same action
    Add -C for custom commands

--- CiscoToolKit.py
+++ CiscoToolKit.py
@@ -1,50 +1,101 @@
 import argparse
-from ftp_download import ftp_downloading
+import ftp_download
 import GetSwitchVersion
 import ciscoupgradedevice
 import sites
+import customCiscoCommands
 import time
-
+from preauth import authcheck
 def main():
     
     parser = argparse.ArgumentParser(
-        prog="CiscoToolKit",
-        description="A group of tools for managing Cisco network hardware",
-        epilog="Version 0.1"
+        prog="CiscoToolKit.py",
+        description="A group of tools for managing Domain network hardware",
+        epilog="Version 1.0",
+        
     )
 
     parser.add_argument('-d', '--download', action='store_true', help="Download the binary file used for upgrading the device.")
     parser.add_argument('-u', '--upgrade', action='store_true', help="Upgrade the Cisco Device; Binary File must already be installed")
-    parser.add_argument('-A', '--letsdoitlive', action='store_true', help="Do IT ALL; download, upgrade, show version currently not working")
     parser.add_argument('-V', '--versions', action='store_true', help="Show Persistent Version Information")
+    parser.add_argument('-C', '--command', action='store_true', help="Run Custom Cisco Device Commands on hosts in sites.py")
     args = parser.parse_args()
 
     if args.upgrade:
-        hosts = sites.targets()
-        site = sites.site()
-        print(hosts)
-        print("Upgrade\nCancel Now? 'CTRL+C'")
-        binary, username, password = sites.credential()
-        ciscoupgradedevice.upgrade_multiple_devices(site,binary,username,password,hosts)
+        try:
+            hosts = sites.choose_hosts()
+            site = sites.site()
+            binary, username, password = sites.credential()
+            print(hosts)
+            if not hosts:
+                print("You've selected no hosts.\n\n")
+                exit()
+            #hosts = sites.targets()
+            else:
+                try:
+                    authchecking = authcheck(username,password,hosts)
+                    #print(authchecking)
+                    if authchecking == True:
+                        print("Initiating Download...\nCancel Now? 'CTRL+C'")
+                        time.sleep(2)
+                        ciscoupgradedevice.upgrade_multiple_devices(site,binary,username,password,hosts)
+                except:
+                    print("oh well goes the goose")
+                    exit()
+        except Exception as e:
+            print(e)
     elif args.download:
-        hosts = sites.targets()
-        site = sites.site()
-        binary, username, password = sites.credential()
-        print(hosts)
-        print("Download\nCancel Now? 'CTRL+C'")
-        time.sleep(3)
-        for host in hosts:
-            ftp_downloading(site,binary,username,password,host)
+        try:
+            hosts = sites.choose_hosts()
+            site = sites.site()
+            binary, username, password = sites.credential()
+            print(hosts)
+            if not hosts:
+                print("You've selected no hosts.\n\n")
+                exit()
+        #hosts = sites.targets()
+            else:
+                try:
+                    authchecking = authcheck(username,password,hosts)
+                    #print(authchecking)
+                    if authchecking == True:
+                        print("Initiating Download...\nCancel Now? 'CTRL+C'")
+                        time.sleep(2)
+                        ftp_download.download_multiple_devices(site,binary,username,password,hosts)
+                except Exception as e:
+                    print(f"oh well goes the goose\n{e}")
+                    exit()
+        except Exception as e:
+            print(e)
     elif args.versions:
         username, password = GetSwitchVersion.display_initial_text()
         GetSwitchVersion.initialize_globals(username, password)
         GetSwitchVersion.verify_credentials(GetSwitchVersion.hosts, GetSwitchVersion.cisco_router)
-        GetSwitchVersion.run_curses_interface()
-    elif args.letsdoitlive:
-        print(hosts)
-        print("LETS DO IT LIVE!\nCancel Now? 'CTRL+C'")
-        time.sleep(3)
-        ciscoupgradedevice.upgrade_multiple_devices(site,binary,username,password,hosts)
+        GetSwitchVersion.run_curses_interface()      # Assuming GetSwitchVersion has a main function to show versions
+    elif args.command:
+        try:
+            hosts = sites.choose_hosts()
+            if not hosts:
+                print("You've selected no hosts.\n\n")
+                exit()
+        #hosts = sites.targets()
+            else:
+                username, password = sites.c_credential()
+                print(hosts)
+                try:
+                    authchecking = authcheck(username,password,hosts)
+                    #print(authchecking)
+                    if authchecking == True:
+                        print("Preparing The Command Module...\nCancel Now? 'CTRL+C'")
+                        time.sleep(2)
+                    customCiscoCommands.command_multiple_devices(username, password, hosts)
+                except Exception as e:
+                    print(f"oh well goes the goose: {e}")
+                    exit()
+        except Exception as e:
+            print(f"An Error Occurred While trying to select devices.\n\n{e}")
+            exit()
+
     else:
         parser.print_help()
         
